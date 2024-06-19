23611d7d3e45306ad79ce8e9d19be747292e2a86
CODEITUP007 <129313043+CODEITUP007@users.noreply.github.com>
2024-06-19 20:16:14 +0530

    reactjs

---
+++ ReactJS/project/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>How are you Kushal Krishna</h1>
+      <p>MY NAME IS HEMANTH KUMAR</p>
+    </div>
+  );
+}
+export default App;
